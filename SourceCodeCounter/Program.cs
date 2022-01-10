4729b4bdb1a62a61081fd812ad390519a2dca04d
Adam Vincent <entityadam@outlook.com>
2022-01-10 14:04:39 -0500

    explicit type instead of var

--- SourceCodeCounter/Program.cs
+++ SourceCodeCounter/Program.cs
@@ -39,24 +39,24 @@ static List<ScanFileInfo> CountFilesAndLines(IEnumerable<string> files)
     return result;
 }
 
-static void PrintScanFileInfoMarkdownTable(IOrderedEnumerable<IGrouping<string?, ScanFileInfo>> fileTypeGroups)
+static void PrintScanFileInfoMarkdownTable(IOrderedEnumerable<IGrouping<string?, ScanFileInfo>> fileTypeGroupings)
 {
     Console.WriteLine($"|Extension   |File Count  |Line Count  |Size        |");
     Console.WriteLine($"|------------|------------|------------|------------|");
 
-    foreach (var fileType in fileTypeGroups)
+    foreach (IGrouping<string?, ScanFileInfo> fileTypeGrouping in fileTypeGroupings)
     {
         const int columnWidth = 12;
-        var key = fileType.Key?.ToString() ?? string.Empty;
+        var key = fileTypeGrouping.Key?.ToString() ?? string.Empty;
         StringBuilder stringBuilder = new();
         stringBuilder.Append('|');
         stringBuilder.Append(key.PadRight(columnWidth, ' '));
         stringBuilder.Append('|');
-        stringBuilder.Append(fileType.Count().ToString().PadRight(columnWidth, ' '));
+        stringBuilder.Append(fileTypeGrouping.Count().ToString().PadRight(columnWidth, ' '));
         stringBuilder.Append('|');
-        stringBuilder.Append(fileType.Sum(x => x.LineCount).ToString().PadRight(columnWidth, ' '));
+        stringBuilder.Append(fileTypeGrouping.Sum(x => x.LineCount).ToString().PadRight(columnWidth, ' '));
         stringBuilder.Append('|');
-        stringBuilder.Append(fileType.Sum(x => x.SizeInBytes).ToString().PadRight(columnWidth, ' '));
+        stringBuilder.Append(fileTypeGrouping.Sum(x => x.SizeInBytes).ToString().PadRight(columnWidth, ' '));
         stringBuilder.Append('|');
         Console.WriteLine(stringBuilder.ToString());
     }
